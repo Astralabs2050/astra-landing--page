5e450bc605611b930680f9b6eac1c07a2682ee47
Ijeoma Igbokwe <preciousigbokwe377@gmail.com>
2024-04-05 21:31:04 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import { Helmet } from "react-helmet";
 function App() {
   return (
     <div>
-      <LandingPage />
+      <Helmet>
+        {" "}
+        <meta charSet="utf-8" />
+        <title>Astra: Bring your fashion ideas to life.</title>
+        <meta
+          name="description"
+          content="With Astra, you can generate fashion ideas using AI and find makers nearby to bring it to life"
+        />
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
+      <div>
+        <LandingPage />
+      </div>
     </div>
   );
 }
