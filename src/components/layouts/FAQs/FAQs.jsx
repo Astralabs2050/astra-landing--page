0fdec9e8651a0a6c0bed6e2f0b743500ed5918f8
Ijeoma Igbokwe <129294448+ijay123@users.noreply.github.com>
2024-09-30 09:08:42 +0100

    update on faqs button

--- src/components/layouts/FAQs/FAQs.jsx
+++ src/components/layouts/FAQs/FAQs.jsx
@@ -23,21 +23,21 @@ const FAQs = () => {
           <div className="flex flex-col items-center gap-[30px]">
             <button
               onClick={() => toggleQuestion(1)}
-              className="w-[448px] h-[46px] border-[#D9D9D91A] bg-customGray rounded-[5px] border flex justify-between items-center px-[30px]"
+              className="w-[448px] h-[46px] border-[#D9D9D91A] bg-customGray overflow-y-hidden rounded-[5px] border flex justify-between items-center px-[30px]"
             >
               Who owns the design created?{" "}
               {activeQuestion === 1 ? <FaAngleDown /> : <FaAngleRight />}
             </button>
             <button
               onClick={() => toggleQuestion(2)}
-              className="w-[448px] h-[46px] bg-customGray border-[#D9D9D91A] rounded-[5px] border flex justify-between items-center px-[30px]"
+              className="w-[448px] h-[46px] bg-customGray border-[#D9D9D91A]  overflow-y-hidden rounded-[5px] border flex justify-between items-center px-[30px]"
             >
               Do you vet partners selected?{" "}
               {activeQuestion === 2 ? <FaAngleDown /> : <FaAngleRight />}
             </button>
             <button
               onClick={() => toggleQuestion(3)}
-              className="w-[448px] h-[46px] bg-customGray border-[#D9D9D91A] rounded-[5px] border flex justify-between items-center px-[30px]"
+              className="w-[448px] h-[46px] bg-customGray border-[#D9D9D91A]  overflow-y-hidden rounded-[5px] border flex justify-between items-center px-[30px]"
             >
               What blockchain is this built on?{" "}
               {activeQuestion === 3 ? <FaAngleDown /> : <FaAngleRight />}
